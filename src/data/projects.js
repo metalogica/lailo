505a400318557cf81eb5e6c76aa69dfce5628078
Richard Jarram <rjarram@me.com>
2019-06-10 06:48:07 +0100

    scaffolded index; created data folder

---
+++ src/data/projects.js
@@ -0,0 +1,92 @@
+const projects = [
+  {
+    title: 'Dharma Taylor',
+    description: 'London-based fashion designer Dharma Taylor specialises in menswear.',
+    summary: 'The website allows the user to create blog content with automated meta tags and sitemap generation.',
+    logo: '../../assets/images/asyla-logo.png'
+    stack: [
+      {
+        name: 'Vue JS',
+        icon: '../../assets/images/vue.png'
+      },
+      {
+        name: 'Ruby on Rails',
+        icon: '../../assets/images/rails.png'
+      },
+      {
+        name: 'AWS EC2',
+        icon: '../../assets/images/aws.svg'
+      }
+    ],
+    links: {
+      site: 'www.dharmataylor.com',
+      admin-console: ''
+    }
+  },
+  {
+    title: 'Asyla',
+    description: 'Asyla is a product that intends to address the communication gap existing between refugee resettlement agencies and the refugees they represent.',
+    summary: 'I built the MVP on Rails in a 10 day sprint. I\'m currently re-building the site to include system & fuctional tests, a React frontend, and chat functionality.',
+    stack: [
+      {
+        name: 'Heroku',
+        icon: '../../assets/images/heroku.png'
+      },
+      {
+        name: 'ES6',
+        icon: '../../assets/images/js.png'
+      },
+      {
+        name: 'Ruby on Rails',
+        icon: '../../assets/images/rails.png'
+      }
+    ],
+    links: {
+      site: 'www.asyla.ca'
+    }
+  },
+  {
+    title: 'Collab Machine',
+    description: 'Collab Machine is a freelance collective based in Montréal.',
+    summary: 'I contribute to the development of new features for the online booking platform as well as identifying and fixing bugs.',
+    stack: [
+      {
+        name: 'Vue JS',
+        icon: '../../assets/images/vue.png'
+      },
+      {
+        name: 'ES6',
+        icon: '../../assets/images/js.png'
+      },
+      {
+        name: 'Ruby on Rails',
+        icon: '../../assets/images/rails.png'
+      }
+    ],
+    links: {
+      site: 'www.collabmachine.com'
+    }
+  },
+ {
+    title: 'React Playground',
+    description: 'Happy reacts only.',
+    summary: 'A mini protfolio of web-apps built on React just for fun. As you\'ve probably noticed, this site is built usign React too.',
+    stack: [
+      {
+        name: 'React',
+        icon: '../../assets/images/react.png'
+      },
+      {
+        name: 'Redux',
+        icon: '../../assets/images/redux.png'
+      },
+      {
+        name: 'Github Pages',
+        icon: '../../assets/images/github.png'
+      }
+    ],
+    links: {
+      site: ''
+    }
+  },
+]
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Router
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose }from 'redux';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from'react-router-dom';
+import { createHistory as history } from 'history';
+
+// Middleware
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { reducer as formReducer } from 'redux-form';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middleware = composeEnhancers(applyMiddleware(logger, reduxPromise))
+
+// Components and Containers
+import App from './components/app.jsx';
+
+// DB
+
+// Initial State
+const initialState = {
+};
+
+// Reducers
+const reducers = combineReducers({
+});
+
+//Stylesheets
 import '../assets/stylesheets/application.scss';
 
 const Hello = ({ name }) => {
@@ -12,7 +39,15 @@ const Hello = ({ name }) => {
   );
 };
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers, initialState, middleware)}>
+    <Router history={history}>
+      <Switch>
+        <Route path="/contact" exact component={App}/>
+        <Redirect from="/" to="/contact"/>
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
